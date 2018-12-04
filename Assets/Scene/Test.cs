1eb2277345bf357738a607a3e80d361108c6994c
chenweichao <chen15270994267@gmail.com>
2018-12-04 10:43:59 +0800

    上传讯飞demo

---
+++ Assets/Scene/Test.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using Wangz.IFly;
+
+public class Test : MonoBehaviour
+{
+    private IFlyBase m_ifly;
+    public Button m_start;
+    public Button m_stop;
+    public Text m_result;
+
+    // Use this for initialization
+    void Start()
+    {
+#if UNITY_EDITOR || UNITY_STANDALONE_WIN
+        m_ifly = gameObject.AddComponent<IFlyWin>();
+#elif UNITY_ANDROID
+        m_ifly = gameObject.AddComponent<IFlyAndroid>();
+#elif UNITY_IOS
+        m_ifly = gameObject.AddComponent<IFlyIOS>();
+#endif
+        m_ifly.m_appid = "5c04f723";
+
+        m_start.onClick.AddListener(() =>
+        {
+            m_ifly.StartSpeech();
+        });
+        m_stop.onClick.AddListener(() =>
+        {
+            m_ifly.StopSpeech();
+        });
+        m_ifly.OnResultEvent += (result) =>
+        {
+            m_result.text = result;
+        };
+    }
+}
---
+++ Assets/Scripts/IFlyIOS.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Wangz.IFly
+{
+    public class IFlyIOS : IFlyBase
+    {
+        public override void Init()
+        {
+        }
+
+        public override void StartSpeech()
+        {
+        }
+
+        public override bool isListening()
+        {
+            return false;
+        }
+
+        public override void StopSpeech()
+        {
+        }
+
+        public override void CancelSpeech()
+        {
+        }
+    }
+}
